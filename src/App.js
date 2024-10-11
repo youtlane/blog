b9ed5e6e9507ead7e568fd3d9049107367bf1321
youtlane <youtlanehanane@gmail.com>
2024-10-11 14:37:53 +0200

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter ,Routes ,Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticlePage from "./pages/ArticlePage";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App flex flex-col items-center">
+        <h1 className="text-red-600"> My Awesome blog</h1>
+        <div>
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/about" element={<AboutPage/>}/>
+            <Route path="/articles" element={<ArticleListPage/>}/>
+            <Route path="/articles/:articleId" element={<ArticlePage/>}/>
+            
+            
+          </Routes>
+        </div>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
