99829370dc3d9afd12f960b509be0f12cfc05348
youtlane <youtlanehanane@gmail.com>
2024-10-14 15:00:36 +0200

    feat: add content and style for pages home and about

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,32 @@
-import React from 'react'
-
-export default function AboutPage() {
+const AboutPage = () => {
   return (
-    <div> hello this is the About Page</div>
-  )
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-4xl font-bold text-center mb-6">About Me</h1>
+      <p className="text-lg text-gray-700 mb-4">
+          Welcome to my blog! Proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+          Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+          non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+          eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+          sodales purus euismod.
+      </p>
+      <p className="text-lg text-gray-700 mb-4">
+          Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.
+      </p>
+      <p className="text-lg text-gray-700 mb-4">
+          Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+          Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+          nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+          sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+          interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+          consequat quam. Vivamus accumsan dui in facilisis aliquet.
+      </p>
+    </div>
+  );
 }
+
+export default AboutPage;
