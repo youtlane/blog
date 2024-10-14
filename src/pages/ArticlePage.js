7487a4738d9266e9a45002db22c1b09cb1150eb3
youtlane <youtlanehanane@gmail.com>
2024-10-14 12:21:10 +0200

    feat: display article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
-export default function ArticlePage() {
-  return (
-    <div>ArticlePage</div>
-  )
+const ArticlePage = () =>{
+  const {articleId} = useParams();
+  const article = articles.find(article => article.name === articleId);
+  return(
+    <article className="max-w-4xl mx-auto p-6 m-6 bg-white shadow-lg rounded-lg">
+      <h1 className="text-3xl font-bold text-gray-900 mb-4">{article.title}</h1>
+      {article.content.map((paragraph, index) => (
+        <p key={index} className="text-lg text-gray-700 leading-relaxed mb-4">
+          {paragraph}
+        </p>
+      ))}
+    </article>
+  );
 }
+export default ArticlePage;
