9edc8b46f221243a2d1a2c968b347c68208f4fe9
youtlane <youtlanehanane@gmail.com>
2024-10-14 10:47:59 +0200

    feat: create NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Articles</li>
+            </ul>
+        </nav>
+
+    );
+
+}
+export default NavBar;
