e40ee078a166641f27978c35dff851162e90fcd6
youtlane <youtlanehanane@gmail.com>
2024-10-14 13:08:37 +0200

    feat: display the article list component into the articles page

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ArticleList from '../components/ArticleList';
+import articles from './article-content';
 
-export default function ArticleListPage() {
-  return (
-    <div>hello this is the ArticleList Page</div>
-  )
+
+const ArticleListPage = ()=>{
+  return(
+    <div className="max-w-6xl mx-auto p-6">
+      <h1 className="text-4xl font-bold text-gray-900 mb-8 text-center">Articles</h1>
+      <ArticleList articles={articles}/>
+    </div>
+
+  );
 }
+export default ArticleListPage ;
